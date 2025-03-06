128b400e23c2e84229627ccf291f538a4650b72d
Tyler <tyler.thomas.127@gmail.com>
2025-03-06 07:18:31 -0600

    fixing build

--- src/components/molecules/BottomMenu.vue
+++ src/components/molecules/BottomMenu.vue
@@ -1,7 +1,7 @@
 <script setup lang="ts">
 import { useRoute, useRouter } from 'vue-router'
 import IconButton from '../atoms/IconButton.vue'
-import { onMounted, onUpdated } from 'vue'
+import { onMounted } from 'vue'
 
 const router = useRouter()
 const route = useRoute()
